37a38725e981acfa3cf8436f670c8567de0fa041
Mark Qian <markqian199413@gmail.com>
2016-06-25 23:56:54 -0700

    make code easier to read

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to @user
     else
       redirect_to '/signup'
     end
