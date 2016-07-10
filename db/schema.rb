ac7b2d57e41c15461be9ab73f04141b3642be000
Mark Qian <markqian199413@gmail.com>
2016-07-09 19:03:19 -0700

    delete works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629004020) do
+ActiveRecord::Schema.define(version: 20160710012209) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160629004020) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
 end
