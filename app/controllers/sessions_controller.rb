e9abc136066e1fce38576440b8ac9699240f103a
Mark Qian <markqian199413@gmail.com>
2016-07-16 00:44:16 -0700

    add a 'remeber me' button for login page. profile in welcome page also changed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,16 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       redirect_to '/welcome'
     else
+      # flash.now[:danger] = 'Invalid email/password combination'
       redirect_to '/login'
     end 
   end
   
   def destroy
     log_out
-    # redirect_to root_url
     redirect_to '/'
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710012209) do
+ActiveRecord::Schema.define(version: 20160716055149) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160710012209) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.boolean  "admin"
+    t.string   "remember_digest"
   end
 
 end
