6a84e89dd1d959646bdaa44e22f54ff16b2a6566
nebasnihas <nnebas@sfu.ca>
2016-07-27 18:32:07 -0700

    test

---
+++ app/controllers/library_controller.rb
@@ -0,0 +1,90 @@
+class LibraryController < ApplicationController
+  layout false
+
+  def index
+  end
+  
+  
+helper_method :getLibOpenArray
+helper_method :getFreePCHash
+
+require 'net/http'
+require 'ostruct'
+
+
+#get Library api(Details) and return all the Json strings
+   def getDetails(address)
+    url = URI.parse(address)
+    res = Net::HTTP.get(url)
+    return res
+   end
+   
+#get Library api(Summary) and return all the Json strings
+   def getSummary(address)
+    url = URI.parse(address)
+    res = Net::HTTP.get(url)
+    return res
+   end
+   
+#get Json strings and return parsed hash
+    def parse(api)
+        obj = JSON.parse(api, object_class: OpenStruct)
+        return obj
+    end
+    
+#read parsed hash and return library info as array 
+    def getLibOpenArray(key)
+        returnArray = []
+        obj = parse(getSummary("http://api.lib.sfu.ca/hours/summary"))
+          for i in obj
+            returnArray.push(i[key])
+          end
+         return returnArray
+    end
+    
+   
+    def getFreePCHash(field, lib_pc)
+        obj = parse(getSummary("http://api.lib.sfu.ca/equipment/computers/free_summary"))
+        returnArray = []
+        returnArray.push(obj[field][lib_pc])
+        return returnArray
+    end
+   
+   
+##########################################################################   
+###Code below is for Library detail API #####
+#location field is in Array but others(hours, ranges) are in hash so we need two different
+#methods to retrieve those info
+###
+
+# #retrieve info stored in array []
+# #field ex:"locations"..
+# #key ex: "name"
+
+#     def getArray(field, key)
+#         returnArray = []
+#         obj = parse(getSummary)
+        
+#         for i in obj[field]
+#              returnArray.push(i[key])       
+#         end
+
+#         return returnArray
+#     end
+
+
+# #retrieve info stored in hash{}
+# #field ex:"locations"..
+# #key ex: "name"
+#     def getHash (field, key)
+#         obj = parse(getDetails)
+#         returnArray = []
+#         for i in 1..2
+#             returnArray.push(obj[field][i.to_s][key])
+#         end
+#         return returnArray
+#     end
+##########################################################################       
+
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,42 @@
+Rails.application.routes.draw do
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  root 'users#index'
+
+  get 'maps' => 'maps#index'
+  
+  get 'library' => 'library#index'
+
+  get 'welcome' => 'welcome#home'
+
+  get '/signup'  => 'users#new'
+
+  get 'admin' => 'users#admin'
+  resources :users
+
+  get 'maps/burnaby'
+  get 'maps/surrey'
+  get 'maps/vancouver'
+
+  resources :maps
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+  resources :sessions
+  resources :messages do
+  resources :comments
+  resources :users
+  end
+
+  get 'transit/index'
+  resources :transit
+
+  # Api definition
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    # We are going to list our resources here
+  end
+
+
+end
