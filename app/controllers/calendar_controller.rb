c2a8a2dc8b7df204684f77d93c847ff249ccb4bd
Mark Qian <markqian199413@gmail.com>
2016-07-29 23:45:53 -0700

    controller for calendar changed

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -7,20 +7,20 @@ class CalendarController < ApplicationController
 
 	def redirect
 		client = Signet::OAuth2::Client.new({
-		client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
-		client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+		client_id: ENV.fetch('24035696255-j51rkv134eq2v80d4hp032k7uvfunueg.apps.googleusercontent.com'),
+		client_secret: ENV.fetch('hnyW7rmTk_84ivjU1xP5_XDF'),
 		authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
 		scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
 		redirect_uri: url_for(:action => :callback)
 		})
 
-		redirect_to client.authorization_uri.to_s
+		redirect_to 'http://localhost:3000/calendar/index'
 	end
 
 	def callback
 		client = Signet::OAuth2::Client.new({
-		client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
-		client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+		client_id: ENV.fetch('24035696255-j51rkv134eq2v80d4hp032k7uvfunueg.apps.googleusercontent.com'),
+		client_secret: ENV.fetch('GhnyW7rmTk_84ivjU1xP5_XDF'),
 		token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
 		redirect_uri: url_for(:action => :callback),
 		code: params[:code]
